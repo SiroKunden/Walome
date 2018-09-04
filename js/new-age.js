bddda2809bba1a698b6c3659325813e9b96b5c19
Ondřej Šírek <siro.kunden@gmail.com>
2018-09-04 10:06:30 +0200

    Change program item to get html

--- js/new-age.js
+++ js/new-age.js
@@ -67,9 +67,9 @@ var playlistId = "PL_phu8k3RmzR8p1VmgZLoY1FJ4igmCrqg";
                 var new_item = $(".template").clone();
                 new_item.removeClass("template");
                 new_item.removeClass("hidden");
-                new_item.find(".date").text(item.cas);
-                new_item.find(".post-title").text(item.nadpis);
-                new_item.find(".description").text(item.popis);
+                new_item.find(".date").html(item.cas);
+                new_item.find(".post-title").html(item.nadpis);
+                new_item.find(".description").html(item.popis);
                 new_item.find(".cover").find("div").attr("style", "background-image: url('" + item.id + "'); background-size: cover; height: 250px; background-repeat: no-repeat; background-position: center center;");
 
                 $(".news").append(new_item);
